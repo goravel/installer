0874e0618c2d744cff01be6f1922095bd90ede11
ALMAS <almas1992@126.com>
2025-03-25 17:52:29 +0800

    feat: [#637] goravel/installer supports upgrade by self (#32)
    * feat: [#637] goravel/installer supports upgrade by self
    
    * Update console/commands/upgrade_command_test.go
    
    Co-authored-by: Wenbo Han <hwbrzzl@gmail.com>
    
    ---------
    
    Co-authored-by: Wenbo Han <hwbrzzl@gmail.com>

---
+++ console/commands/upgrade_command.go
@@ -0,0 +1,62 @@
+package commands
+
+import (
+	"errors"
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/goravel/framework/contracts/console"
+	"github.com/goravel/framework/contracts/console/command"
+	"github.com/goravel/framework/support/color"
+	"github.com/goravel/installer/support"
+)
+
+type UpgradeCommand struct {
+}
+
+// Signature The name and signature of the console command.
+func (r *UpgradeCommand) Signature() string {
+	return "upgrade"
+}
+
+// Description The console command description.
+func (r *UpgradeCommand) Description() string {
+	return "Upgrade Goravel installer"
+}
+
+// Extend The console command extend.
+func (r *UpgradeCommand) Extend() command.Extend {
+	return command.Extend{
+		ArgsUsage: " [version]",
+	}
+}
+
+// Handle Execute the console command.
+func (r *UpgradeCommand) Handle(ctx console.Context) error {
+	pkg := support.InstallerModuleName
+	if version := ctx.Argument(0); version != "" {
+		pkg = pkg + "@" + version
+	}
+
+	upgrade := exec.Command("go", "install", pkg)
+	if err := ctx.Spinner(fmt.Sprintf("> @%s", strings.Join(upgrade.Args, " ")), console.SpinnerOption{
+		Action: func() error {
+			output, err := upgrade.CombinedOutput()
+			if err != nil && len(output) > 0 {
+				err = errors.New(strings.TrimSpace(strings.ReplaceAll(string(output), err.Error(), "")))
+			}
+
+			return err
+		},
+	}); err != nil {
+		color.Errorln("Failed to upgrade Goravel installer")
+		color.Red().Println(err.Error())
+
+		return nil
+	}
+
+	color.Successln("Goravel installer has been upgraded successfully")
+
+	return nil
+}
